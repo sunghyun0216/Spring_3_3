3e81aa41d63b5dcfb23bbf8da3b85e527a15535b
sunghyun94 <dog0094s@naver.com>
2021-03-23 15:10:19 +0900

    test3

---
+++ src/test/java/com/iu/s3/ConnectionTest.java
@@ -0,0 +1,25 @@
+package com.iu.s3;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConnectionTest extends MyAbstractTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void connectionTest() throws Exception {
+		assertNotNull(sqlSession);
+	}
+	
+
+}
---
+++ src/test/java/com/iu/s3/sampleMapper.java
@@ -0,0 +1,5 @@
+package com.iu.s3;
+
+public class sampleMapper {
+
+}
